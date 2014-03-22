0f60abf91a48a5e92b890793483b453f6a9ebbea
Panagiotis Grontas <pgrontas@gmail.com>
2014-03-22 16:35:00 +0200

    Version 1

---
+++ ElectionCreator/frmElectionCreator.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ElectionCreator
+{
+    partial class frmElection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtElectionOptions = new System.Windows.Forms.TextBox();
+            this.txtElectionName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtElectionOptions);
+            this.groupBox1.Controls.Add(this.txtElectionName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 177);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Election Data";
+            // 
+            // txtElectionOptions
+            // 
+            this.txtElectionOptions.Location = new System.Drawing.Point(5, 60);
+            this.txtElectionOptions.Multiline = true;
+            this.txtElectionOptions.Name = "txtElectionOptions";
+            this.txtElectionOptions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtElectionOptions.Size = new System.Drawing.Size(346, 111);
+            this.txtElectionOptions.TabIndex = 3;
+            // 
+            // txtElectionName
+            // 
+            this.txtElectionName.Location = new System.Drawing.Point(85, 13);
+            this.txtElectionName.Name = "txtElectionName";
+            this.txtElectionName.Size = new System.Drawing.Size(266, 20);
+            this.txtElectionName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Election Options and Values (Split with Space and NewLine)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Election Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtT);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtN);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtS);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtLength);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(7, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(365, 86);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Key Data";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(267, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "to reassemble";
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(205, 60);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(56, 20);
+            this.txtT.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "parts and require t";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(54, 61);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(56, 20);
+            this.txtN.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Split to n ";
+            // 
+            // txtS
+            // 
+            this.txtS.Location = new System.Drawing.Point(132, 38);
+            this.txtS.Name = "txtS";
+            this.txtS.Size = new System.Drawing.Size(104, 20);
+            this.txtS.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Select s:";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(132, 13);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(219, 20);
+            this.txtLength.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Select Key Length (Bits)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(7, 287);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(365, 127);
+            this.txtLog.TabIndex = 4;
+            // 
+            // frmElection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 455);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmElection";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Election Creation Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtElectionOptions;
+        private System.Windows.Forms.TextBox txtElectionName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ElectionCreator/frmElectionCreator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+using System.Data.SQLite;
+using CryptoLib;
+using Microsoft.FSharp.Core;
+
+namespace ElectionCreator
+{
+    public partial class frmElection : Form
+    {
+        public frmElection()
+        {
+            InitializeComponent();
+        }
+
+        void log(string msg)
+        {
+            txtLog.AppendText(msg + "\n");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initDB();
+                var eid = insertElection();
+                log("Created Election" + eid);
+                var cnds = insertElectionOptions(eid);
+                log("Created Election Options For " + eid);
+                insertKey(eid,cnds);
+                log("Success");
+            }
+            catch (Exception ex)
+            {
+                log(ex.ToString());
+            }
+           
+        }
+
+        SQLiteConnection dbConn;
+        private void initDB()
+        {
+            var connString = @"Data Source=C:\Users\Panagiotis\Dropbox\Universities\ΜΠΛΑ\Διπλωματική\Code\DJN\djn.db;Version=3;";
+            dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+        }
+
+        private Guid insertElection()
+        {
+            var desc = txtElectionName.Text.Trim();
+            var cmd = dbConn.CreateCommand();
+            var eID = System.Guid.NewGuid();
+            cmd.CommandText = " INSERT INTO Elections (ID, Description) " +
+                              " VALUES (@ID, @Desc) ";
+            cmd.Parameters.AddWithValue("@ID", eID);
+            cmd.Parameters.AddWithValue("@Desc", desc);
+       
+            cmd.ExecuteNonQuery();
+
+            return eID;
+        }
+
+        private int insertElectionOptions(Guid eid)
+        {
+            var cmd = dbConn.CreateCommand();
+            var options = txtElectionOptions.Text.Trim();
+            var c = Environment.NewLine + "\t";
+            var ls = options.Split(c.ToCharArray());
+            int i = 0;
+            foreach (var l in ls)
+            {
+                if (l==string.Empty) continue;
+                var opt = l.Split( new char[]{' '} );
+                cmd.CommandText = " INSERT INTO ElectionOptions (ID, Label, Value, ElectionID) " +
+                              " VALUES (@ID, @Label, @Value, @ElectionID) ";
+                cmd.Parameters.AddWithValue("@ID", System.Guid.NewGuid());
+                cmd.Parameters.AddWithValue("@Label", opt[0]);
+                cmd.Parameters.AddWithValue("@Value", Int32.Parse(opt[1]));
+                cmd.Parameters.AddWithValue("@ElectionID", eid.ToString());
+                cmd.ExecuteNonQuery();
+                i++;
+            }
+            return i;
+        }
+
+        private void insertKey(Guid eid,int cnds)
+        {
+            var k = Int32.Parse(txtLength.Text.Trim());
+            var s = Int32.Parse(txtS.Text.Trim());
+            var n = Int32.Parse(txtN.Text.Trim());
+            var t = Int32.Parse(txtT.Text.Trim());
+            
+            var cr = new Paillier.ThresPaillier(t, n);
+            var keys = cr.KeyShareGen(k, s);
+            var pk = keys.Item1;
+            var secretKeys = keys.Item2;
+            int i=1;
+            foreach (var sk in secretKeys) {
+                System.IO.File.WriteAllLines("sk" + i + ".txt", new string[] { sk.ToString() });
+                log("Secret Key="+sk.ToString());
+                i++;
+            }
+
+            var M = BigInteger.Pow (2, (k*s)/cnds+1);
+
+            var cmd = dbConn.CreateCommand();
+            cmd.CommandText = " INSERT INTO PublicKeys (ID, n, s, D, ssp, Type, numShares, numThreshold, M, ElectionID) " +
+                              " VALUES (@ID, @n, @s,@D, @ssp, @Type, @ns, @nt, @M, @ElectionID) ";
+            cmd.Parameters.AddWithValue("@ID", System.Guid.NewGuid());
+            cmd.Parameters.AddWithValue("@n", pk.Item1);
+            cmd.Parameters.AddWithValue("@s", pk.Item2);
+            cmd.Parameters.AddWithValue("@D", pk.Item3);
+            cmd.Parameters.AddWithValue("@ssp", pk.Item4);
+            cmd.Parameters.AddWithValue("@ns", n);
+            cmd.Parameters.AddWithValue("@nt", t);
+             cmd.Parameters.AddWithValue("@M", M);
+            cmd.Parameters.AddWithValue("@Type", 0);
+            cmd.Parameters.AddWithValue("@ElectionID", eid.ToString());
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
